46a9e8ec9bdbd7eebc9808fbb92da004a5fb2c22
dev-front <caroline.hugues@lean.fr>
2021-05-25 17:23:27 +0200

    component header created

---
+++ vue-cli/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <h1>{{ title }}</h1>
+        <p>Pour plus de renseignement : <a :href="moreInfos.link" target="_blank">{{ moreInfos.title }}</a></p>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data: function() {
+    return {
+      title: 'Randonnées à La Sainte-Victoire',
+      moreInfos: {
+        title: 'Grand site Sainte-Victoire', 
+        link: 'http://www.grandsitesaintevictoire.com/'
+      },
+    };
+  },
+  created: function() {
+      document.title = 'Randonnées à la Sainte-Victoire'
+  }
+}
+</script>
